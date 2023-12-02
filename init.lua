7cb6f580fe4d44fbf45bd6191eb8b5a1d40ba970
yobibyte <vitaliykurin@gmail.com>
2023-12-02 12:39:52 +0000

    Add symbols outlinew

--- init.lua
+++ init.lua
@@ -28,6 +28,7 @@ require('lazy').setup({
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
+  'simrat39/symbols-outline.nvim',
 
   {
     'stevearc/conform.nvim',
@@ -669,7 +670,73 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
-
+local opts = {
+  highlight_hovered_item = true,
+  show_guides = true,
+  auto_preview = false,
+  position = 'right',
+  relative_width = true,
+  width = 25,
+  auto_close = false,
+  show_numbers = false,
+  show_relative_numbers = false,
+  show_symbol_details = true,
+  preview_bg_highlight = 'Pmenu',
+  autofold_depth = nil,
+  auto_unfold_hover = true,
+  fold_markers = { '+', '-' },
+  wrap = false,
+  keymaps = { -- These keymaps can be a string or a table for multiple keys
+    close = { "<Esc>", "q" },
+    goto_location = "<Cr>",
+    focus_location = "o",
+    hover_symbol = "<C-space>",
+    -- toggle_preview = "K",
+    rename_symbol = "r",
+    code_actions = "a",
+    fold = "h",
+    unfold = "l",
+    fold_all = "W",
+    unfold_all = "E",
+    fold_reset = "R",
+  },
+  lsp_blacklist = {},
+  symbol_blacklist = {
+    'Variable',
+  },
+  symbols = {
+    File = { icon = "F", hl = "@text.uri" },
+    Module = { icon = "M", hl = "@namespace" },
+    Namespace = { icon = "N", hl = "@namespace" },
+    Package = { icon = "P", hl = "@namespace" },
+    Class = { icon = "C", hl = "@type" },
+    Method = { icon = "f", hl = "@method" },
+    Property = { icon = "p", hl = "@method" },
+    Field = { icon = "fi", hl = "@field" },
+    Constructor = { icon = "c", hl = "@constructor" },
+    Enum = { icon = "e", hl = "@type" },
+    Interface = { icon = "i", hl = "@type" },
+    Function = { icon = "f", hl = "@function" },
+    Variable = { icon = "v", hl = "@constant" },
+    Constant = { icon = "c", hl = "@constant" },
+    String = { icon = "s", hl = "@string" },
+    Number = { icon = "n", hl = "@number" },
+    Boolean = { icon = "b", hl = "@boolean" },
+    Array = { icon = "a", hl = "@constant" },
+    Object = { icon = "o", hl = "@type" },
+    Key = { icon = "k", hl = "@type" },
+    Null = { icon = "", hl = "@type" },
+    EnumMember = { icon = "e", hl = "@field" },
+    Struct = { icon = "s", hl = "@type" },
+    Event = { icon = "e", hl = "@type" },
+    Operator = { icon = "+", hl = "@operator" },
+    TypeParameter = { icon = "t", hl = "@parameter" },
+    Component = { icon = "c", hl = "@function" },
+    Fragment = { icon = "f", hl = "@constant" },
+  },
+}
+require("symbols-outline").setup(opts)
+vim.keymap.set('n', "<leader>o", ":SymbolsOutline<CR>")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
